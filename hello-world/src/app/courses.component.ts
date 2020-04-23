9005df5d72df989b3aa87e9c801f3b0ca0cc1c60
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-04-22 20:45:34 -0400

    Registered new Component in app

---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses',
+  template: '<h2>Courses</h2>'
+})
+//basic CSS knowledge about selector
+//courses  -> <courses/>
+//.courses -> <div class=courses/>
+//#courses -> <div id=courses/>
+export class CoursesComponent {
+
+}
