255c34ec51f258b5f980269aaad90df31ae30995
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-04-22 21:06:23 -0400

    changed indent size and added member to courses component

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'courses',
-  template: '<h2>Courses</h2>'
+    selector: 'courses',
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{course}}
+            </li>
+        </ul>
+    `
 })
 //basic CSS knowledge about selector
 //courses  -> <courses/>
 //.courses -> <div class=courses/>
 //#courses -> <div id=courses/>
-export class CoursesComponent {
-
+export class CoursesComponent
+{
+    title = "List of courses";
+    courses = ["course1", "course2", "course3"]
 }
