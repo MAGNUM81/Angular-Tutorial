34fffde7ff7a00c4dddb4613ef81e59a7ea6f6fd
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-05-04 22:48:31 -0400

    Pipes
    string, number, decimal, currency, date, ...

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,7 +4,11 @@ import {Component, OnInit} from '@angular/core';
     selector: 'courses',
     //#email : template variable
     template: `
-        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
+        {{ course.title | uppercase | lowercase }} <br/>
+        {{ course.students | number }} <br/>
+        {{ course.rating | number:'2.1-1'}} <br/>
+        {{ course.price | currency:'CAD':true:'3.2-2' }} <br/>
+        {{ course.releaseDate | date:'dd-MM-yyyy' }} <br/> //angular.io -> datepipe
     `
 })
 //basic CSS knowledge about selector
@@ -13,9 +17,11 @@ import {Component, OnInit} from '@angular/core';
 //#courses -> <div id=courses/>
 export class CoursesComponent
 {
-    email = "me@example.com";
-    onKeyUp()
-    {
-        console.log(this.email)
+    course = {
+        title: "Some Very Interesting Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016,3,1, 12, 34)
     }
 }
