7573fceba201d7091a2d478449ad8b3ed0d784b1
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-05-04 22:28:44 -0400

    Template variables

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,14 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {CoursesService} from "./courses.service";
 
 @Component({
     selector: 'courses',
-    //DOM style object on w3School
+    //#email : template variable
     template: `
-        <div (click)="onDivClicked()">
-            <button (click)="onSave($event)">Save</button>
-        </div>
-
+        <input #email (keyup.enter)="onKeyUp(email.value)"/>
     `
 })
 //basic CSS knowledge about selector
@@ -17,15 +13,8 @@ import {CoursesService} from "./courses.service";
 //#courses -> <div id=courses/>
 export class CoursesComponent
 {
-    onSave($event)
+    onKeyUp(email)
     {
-        $event.stopPropagation(); //this call stops an event from bubbling further up the tree
-        console.log("Button was clicked", $event);
-    }
-
-    onDivClicked()
-    {
-        //Clicking the button will make the event bubble up the DOM tree
-        console.log("Div was clicked")
+        console.log(email)
     }
 }
