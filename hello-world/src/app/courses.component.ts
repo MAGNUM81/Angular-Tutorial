16eb5497bc11c42e317490fcc0aed578021fedb9
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-05-05 00:28:57 -0400

    Custom pipes

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,11 +4,7 @@ import {Component, OnInit} from '@angular/core';
     selector: 'courses',
     //#email : template variable
     template: `
-        {{ course.title | uppercase | lowercase }} <br/>
-        {{ course.students | number }} <br/>
-        {{ course.rating | number:'2.1-1'}} <br/>
-        {{ course.price | currency:'CAD':true:'3.2-2' }} <br/>
-        {{ course.releaseDate | date:'dd-MM-yyyy' }} <br/> //angular.io -> datepipe
+        {{ text  | summary:10 }}
     `
 })
 //basic CSS knowledge about selector
@@ -17,11 +13,7 @@ import {Component, OnInit} from '@angular/core';
 //#courses -> <div id=courses/>
 export class CoursesComponent
 {
-    course = {
-        title: "Some Very Interesting Course",
-        rating: 4.9745,
-        students: 30123,
-        price: 190.95,
-        releaseDate: new Date(2016,3,1, 12, 34)
-    }
+    text = `
+    dslfkjbsdfflksjd sdf;oifdhgosdihg ds;oifhdsf. fdsihfsdlkfsd., lksdjifhdslkffdghshg
+    `
 }
