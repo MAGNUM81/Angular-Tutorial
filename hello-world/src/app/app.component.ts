5c742aeb3efde02d828cbdebad2bb575f2f2242e
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-05-05 20:35:14 -0400

    intro trackby

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -7,15 +7,20 @@ import {LikeChangedEventArgs} from "./like/like.component";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    courses = [
-      {id: 1, name: "course1"},
-      {id: 2, name: "course2"},
-      {id: 3, name: "course3"}
-    ];
+   courses;
 
   onAdd()
   {
       this.courses.push({id: 4, name: "course4"})
+  }
+
+  loadCourses() {
+    // this makes Angular reconstruct the whole tree list everytime we call loadCourses
+      this.courses = [
+          {id: 1, name: "course1"},
+          {id: 2, name: "course2"},
+          {id: 3, name: "course3"}
+      ];
   }
 
   onChange(course)
