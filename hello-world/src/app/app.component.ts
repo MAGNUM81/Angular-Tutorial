74c2a27ffe9a7112a34df2afd8abdbdb5af6480d
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-05-05 20:05:23 -0400

    old ngIf syntax (pre-Angular 4)

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -7,15 +7,5 @@ import {LikeChangedEventArgs} from "./like/like.component";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'hello-world';
-    tweet = {
-      body: 'Body of a great great tweet...',
-      likesCount: 8,
-      isLiked: false
-    };
-
-    onLikeChanged(eventArgs: LikeChangedEventArgs)
-    {
-        console.log(eventArgs)
-    }
+    courses = []
 }
