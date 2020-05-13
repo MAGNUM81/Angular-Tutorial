0fb1f5a57a04af9959fdf04ec5e339aa561fb0aa
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-05-13 17:42:26 -0400

    FormBuilder vs Form object
    whoops

--- hello-world/src/app/new-course-form/new-course-form.component.ts
+++ hello-world/src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -7,22 +7,24 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent {
-  form = new FormGroup({
+  form;/* = new FormGroup({
+    name: new FormControl('', Validators.required),
+    contact: new FormGroup({
+      email: new FormControl(),
+      phone: new FormControl()
+    }),
     topics: new FormArray([])
-  })
+  })*/
 
-  addTopic(topic: HTMLInputElement) {
-    this.topics.push(new FormControl(topic.value))
-    topic.value = ''
-  }
-
-  get topics() : FormArray{
-    return this.form.get('topics') as FormArray
-  }
-
-  removeTopic(topic: FormControl) {
-    let index = this.topics.controls.indexOf(topic);
-    this.topics.removeAt(index);
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    })
   }
 }
 
