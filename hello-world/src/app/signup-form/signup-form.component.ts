7a11786dc2ded331130d7375d5452c0317f83af6
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-05-13 17:15:45 -0400

    Nested FormGroups

--- hello-world/src/app/signup-form/signup-form.component.ts
+++ hello-world/src/app/signup-form/signup-form.component.ts
@@ -9,16 +9,14 @@ import {UsernameValidators} from './username.validators';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      UsernameValidators.cannotContainSpace,
-      Validators.minLength(3)],
-      UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   });
 
   get username(){
-    return this.form.get('username')
+    return this.form.get('account.username')
   }
 
   login() {
