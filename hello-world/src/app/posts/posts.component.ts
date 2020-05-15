e1d5d3277f437e75eefeb4ea3422c08510719ce6
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-05-15 18:12:47 -0400

    creating and updateing data

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -8,9 +8,28 @@ import {HttpClient} from '@angular/common/http';
 })
 export class PostsComponent {
   posts: any[]
-  constructor(http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(response => {
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: HttpClient) {
+
+    http.get(this.url).subscribe(response => {
       this.posts = response as any[]
     })
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = {title: input.value};
+    input.value = ''
+    this.http.post(this.url, post)
+      .subscribe(response => {
+        post['id'] = response['id']
+        this.posts.splice(0, 0, post)
+      });
+  }
+
+  updatePost(post: any) {
+    this.http.patch(this.url + '/' + post.id, {isRead: true})
+      .subscribe(response => {
+        console.log(response)
+      })
   }
 }
