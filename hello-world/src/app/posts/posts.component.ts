c8af833c3acf054b4a414863dc3f405e33d54711
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-05-20 16:46:12 -0400

    handling unexpected errors
    this is basic stuff. not usable in production. Use HttpInterceptors instead

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -17,7 +17,10 @@ export class PostsComponent implements OnInit{
   ngOnInit(): void {
     this.postService.getPosts().subscribe(response => {
       this.posts = response as any[]
-    })
+    },
+      (error: Response) => {
+        alert("An unexpected error occurred")
+      })
   }
 
   createPost(input: HTMLInputElement) {
@@ -27,22 +30,42 @@ export class PostsComponent implements OnInit{
       .subscribe(response => {
         post['id'] = response['id']
         this.posts.splice(0, 0, post)
-      });
+      },
+        (error: Response) => {
+        if (error.status === 404){
+          // this.form.setErrors(error)
+        }
+        else{
+          alert("An unexpected error occurred")
+        }
+
+        });
   }
 
   updatePost(post: any) {
     this.postService.updatePost(post, {isRead: true})
-      .subscribe(response => {
-        console.log(response)
-      })
+      .subscribe(
+        response => {
+          console.log(response)
+        },
+        (error: Response) => {
+          alert("An unexpected error occurred")
+        })
   }
 
   deletePost(post: any) {
-    this.postService.deletePost(post.id)
+    this.postService.deletePost(355)
       .subscribe(response => {
         let index = this.posts.indexOf(post)
         this.posts.splice(index, 1)
-      })
-
+      },
+        (error: Response) => {
+          if (error.status === 404) {
+            alert("This post has already been deleted.")
+          }
+          else{
+            alert("An unexpected error occurred")
+          }
+        });
   }
 }
