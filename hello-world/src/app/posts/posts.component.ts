473e1799a11bc9ec4907155d77d22caef424fae5
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-05-20 15:56:48 -0400

    deleting data

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -6,12 +6,16 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit{
+
   posts: any[]
   private url = 'https://jsonplaceholder.typicode.com/posts';
-  constructor(private http: HttpClient) {
 
-    http.get(this.url).subscribe(response => {
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe(response => {
       this.posts = response as any[]
     })
   }
@@ -31,5 +35,14 @@ export class PostsComponent {
       .subscribe(response => {
         console.log(response)
       })
+  }
+
+  deletePost(post: any) {
+    this.http.delete(this.url + "/" + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post)
+        this.posts.splice(index, 1)
+      })
+
   }
 }
