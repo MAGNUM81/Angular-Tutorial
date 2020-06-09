b622b6b5399172f63c584f9aee64487f862870ab
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-06-09 14:32:06 -0400

    nothing work fr
    wtf

--- firebase-demo/src/app/app.component.ts
+++ firebase-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'firebase-demo';
+  courses: any[] = [];
+
+  constructor(db: AngularFirestore) {
+    db.collection('/courses').get().subscribe(data => {
+      console.log(data.docs);
+      this.courses.push(data);
+    });
+  }
 }
--- firebase-demo/src/environments/environment.ts
+++ firebase-demo/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCDhpqgE1BfoCZfQvFAKrOlr1Q4c4ntAdo',
+    authDomain: 'fir-demo-79ff5.firebaseapp.com',
+    databaseURL: 'fir-demo-79ff5',
+    projectId: 'fir-demo-79ff5',
+    storageBucket: 'fir-demo-79ff5.appspot.com',
+    messagingSenderId: '1087897952461',
+    appId: '1:1087897952461:web:43b85ee4826a8702c64984'
+  }
 };
 
 /*
