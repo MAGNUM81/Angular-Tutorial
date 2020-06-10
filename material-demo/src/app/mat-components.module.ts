659a1b9902866dec942cf7a17fe5571c566c8a03
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-06-10 14:43:47 -0400

    refactor modules

--- material-demo/src/app/mat-components.module.ts
+++ material-demo/src/app/mat-components.module.ts
@@ -1,13 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatRadioModule} from '@angular/material/radio';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
@@ -19,23 +14,24 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatDialogModule} from '@angular/material/dialog';
-import {EditCourseComponent} from './edit-course/edit-course.component';
-import {MatComponentsModule} from './mat-components.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EditCourseComponent
-  ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatComponentsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    CommonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatDialogModule
+  ]
 })
-export class AppModule { }
+export class MatComponentsModule { }
