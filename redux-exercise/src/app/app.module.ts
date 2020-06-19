62f728294f2624f344e22c380dcbbd7d6c580573
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-06-19 17:22:14 -0400

    wip on exercise

--- redux-exercise/src/app/app.module.ts
+++ redux-exercise/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import {TodoService} from './todo.service';
+import {NgRedux} from '@angular-redux/store';
+import {AppState, INITIAL_STATE, rootReducer} from './store';
 
 @NgModule({
   declarations: [
@@ -22,4 +24,8 @@ import {TodoService} from './todo.service';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<Readonly<AppState>>){
+    ngRedux.configureStore(rootReducer, INITIAL_STATE)
+  }
+}
--- redux-exercise/src/app/store.ts
+++ redux-exercise/src/app/store.ts
@@ -1,13 +1,33 @@
 import {Time} from '@angular/common';
+import {actions} from './actions';
 
 export interface AppState {
   lastUpdateTime: Time
   totalItems: number
-  todos: any[]
+  todos: TodoItem[]
 }
 
-export function rootReducer(state: Readonly<AppState>, action: Readonly<string>) {
-  switch (action.type) {
+export interface TodoItem {
+  name: string,
+  toggled: boolean
+}
 
+export const INITIAL_STATE : AppState = {
+  lastUpdateTime : null,
+  totalItems: 0,
+  todos: []
+}
+
+export function rootReducer(state: Readonly<AppState>, action: Readonly<any>) {
+  switch (action.type) {
+    case actions.ADD_TODO: return {...state, lastUpdateTime: new Date(), totalItems: state.totalItems + 1, todos: [...state.todos /*, <that new item we're tryna add?>*/]}
+      break;
+    case actions.REMOVE_TODO:
+      break;
+    case actions.TOGGLE_TODO:
+      break;
+    case actions.REMOVE_ALL_TODOS:
+      break;
   }
+  return state;
 }
