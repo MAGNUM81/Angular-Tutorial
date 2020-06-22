d30c9f2de81ddae067749bc40ae2825e632f4ebd
Marc-André Gill-Miville <marc-andre.gill-miville@ibwave.com>
2020-06-22 13:48:01 -0400

    should work.

--- redux-exercise/src/app/store.ts
+++ redux-exercise/src/app/store.ts
@@ -2,9 +2,8 @@ import {Time} from '@angular/common';
 import {actions} from './actions';
 
 export interface AppState {
-  lastUpdateTime: Time
+  lastUpdateTime: Date
   totalItems: number
-  todos: TodoItem[]
 }
 
 export interface TodoItem {
@@ -15,19 +14,14 @@ export interface TodoItem {
 export const INITIAL_STATE : AppState = {
   lastUpdateTime : null,
   totalItems: 0,
-  todos: []
 }
 
 export function rootReducer(state: Readonly<AppState>, action: Readonly<any>) {
   switch (action.type) {
-    case actions.ADD_TODO: return {...state, lastUpdateTime: new Date(), totalItems: state.totalItems + 1, todos: [...state.todos /*, <that new item we're tryna add?>*/]}
-      break;
-    case actions.REMOVE_TODO:
-      break;
-    case actions.TOGGLE_TODO:
-      break;
-    case actions.REMOVE_ALL_TODOS:
-      break;
+    case actions.ADD_TODO: return {...state, lastUpdateTime: new Date(), totalItems: state.totalItems + 1, }
+    case actions.REMOVE_TODO: return {...state, lastUpdateTime: new Date(), totalItems: state.totalItems - 1}
+    case actions.TOGGLE_TODO: return {...state, lastUpdateTime: new Date() }
+    case actions.REMOVE_ALL_TODOS: return {...state, lastUpdateTime: new Date(), totalItems: 0 }
   }
   return state;
 }
--- redux-exercise/src/app/todo-dashboard/todo-dashboard.component.ts
+++ redux-exercise/src/app/todo-dashboard/todo-dashboard.component.ts
@@ -1,40 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { TodoService } from '../todo.service';
+import {NgRedux, select} from '@angular-redux/store';
+import {AppState} from '../store';
+import {actions} from '../actions';
 
 @Component({
   selector: 'app-todo-dashboard',
   templateUrl: './todo-dashboard.component.html',
   styleUrls: ['./todo-dashboard.component.css']
 })
-export class TodoDashboardComponent {
-  todos: number; 
-  lastUpdate; 
-  
+export class TodoDashboardComponent implements OnInit {
+  @select((s: AppState) => s.totalItems) totalTodos;
+  @select((s: AppState) => s.lastUpdateTime) lastUpdate;
+
   // Read the comment in TodoService
-  constructor(private service: TodoService) { 
-    this.todos = service.getTodos().length;
-    
-    service.todoAdded.subscribe(() => { 
-      this.todos++;
-      this.lastUpdate = new Date();
+  constructor(private service: TodoService, private ngRedux: NgRedux<AppState>) {
+    service.todoAdded.subscribe(() => {
+      this.ngRedux.dispatch({type: actions.ADD_TODO})
     });
 
     service.todoRemoved.subscribe(() => {
-      this.todos--;
-      this.lastUpdate = new Date();
+      this.ngRedux.dispatch({type: actions.REMOVE_TODO})
     });
 
     service.todoToggled.subscribe(() => {
-      this.lastUpdate = new Date();
+      this.ngRedux.dispatch({type: actions.TOGGLE_TODO})
     });
 
     service.todosCleared.subscribe(() => {
-      this.todos = 0; 
-      this.lastUpdate = new Date();
+      this.ngRedux.dispatch({type: actions.REMOVE_ALL_TODOS})
     });
   }
 
   clearTodos() {
     this.service.clearTodos();
+  }
+
+  ngOnInit(): void {
+    this.totalTodos = this.ngRedux.getState().totalItems;
   }
 }
