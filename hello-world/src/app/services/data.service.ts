c7983f0a3ea171e78826d5c478eb680c9abd78ed
Marc-André Gill <marc-andre.gill-miville@ibwave.com>
2021-08-30 11:49:03 -0400

    interviewQuestion
    initial

--- hello-world/src/app/services/data.service.ts
+++ hello-world/src/app/services/data.service.ts
@@ -1,22 +1,37 @@
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
+import {Component, Injectable, NgModule, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
 
+@Injectable()
 export class DataService {
-  constructor(private url: string, private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
-  getAll() {
-    return this.httpClient.get(this.url).pipe(map((r) => r))
-  }
-
-  create(resource) {
-    return this.httpClient.post(this.url, JSON.stringify(resource))
-  }
-
-  delete(id) {
-    return this.httpClient.delete(this.url + "/" + id)
-  }
-
-  update(resource){
-    return this.httpClient.patch(this.url + "/" + resource.id, resource)
+  get(url: string): Observable<any> {
+    return this.httpClient.get(url).pipe(tap(r => {
+      console.log('I am a data service');
+      r.hello = 'world';
+    }));
   }
 }
+
+@Component({
+  selector: 'app-root',
+  template: ``
+})
+export class AppRootComponent implements OnInit {
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit(): void {
+    const r: any = this.dataService.get('http://api.example.com/Examples');
+    console.log(r.hello);
+    console.log('AppRootComponent initialized');
+  }
+}
+
+@NgModule({
+  declarations: [AppRootComponent],
+  providers: [DataService]
+})
+export class AppModule{}
